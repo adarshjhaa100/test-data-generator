2f2bddf3f07bc0cc491052d83d3e35617bf79540
adarshjhaa100 <31096082+adarshjhaa100@users.noreply.github.com>
2022-01-23 22:18:03 +0530

    Add logic to generate simple numeric and decimal cols

--- Readme.md
+++ Readme.md
@@ -7,7 +7,20 @@
    <li>Range</li>
    <li>Variance etc.</li>
    </ul>  
-
+   <br/>
+   <b>Progress:<b> Completed logic to generate simple columns(string, float, int) separately. 
+   <br/> 
+   Goto "docs/workflow_design.txt" for the workflow design and column configuration information.
+   <br/>
+   To generate columns, do the following:
+   <ol>
+   <li> Switch directory to generatorLogicScripts using <code>cd generatorLogicScripts</code></li>
+   <l1> Add column config to 'sampleconfigs\simpleconfig1.json' according to config information present in "docs/workflow_design.txt"</li>
+   <l1> Create a folder named sampleoutput inside generatorLogicScripts </li>
+   <l1> Modify the NUM_ROWS variable in generatesimplecol.py with the number of rows requried </li>
+   <l1> Run the file generatesimplecol.py using <code> python generatesimplecol.py</code> </li>
+   </ol>
+   
    In case requirement do not match, user can pass a custom script to generate the specific column, it will be saved as a python file in the **/col_scripts directory.  
 </p>
 
--- generatorLogicScripts/generatesimplecol.py
+++ generatorLogicScripts/generatesimplecol.py
@@ -3,6 +3,7 @@ from simplerandomgenerator import SimpleRandomGenerator
 from datetime import datetime
 import time
 
+NUM_ROWS=20000
 
 class SimpleColConfig():
     
@@ -22,21 +23,25 @@ class SimpleColConfig():
         self.template = jsonconfig['template']
         self.custom = jsonconfig['custom']
         self.noofrows=int(noofrows)
+        self.precision=5 # TODO: need to add field for this
         self.rng=SimpleRandomGenerator()
 
     def construct_generator(self):
+        print(self.type, self.subtype)
         if(self.type.upper()=="NUMERIC"):
             print("numeric")
+            self.generateint()
         if(self.type.upper()=="ALPHANUMERIC"):
             print("alphanumneric")    
             self.generatestring()
         if(self.type.upper()=="DECIMAL"):
-            if(self.subtype=="FLOAT"):
-                self.generatefloat()
-                
+            if(self.subtype.upper()=="FLOAT"):
+                print("FLOAT")
+                self.generatefloat()             
     
     def generatestring(self):
-        fromlist=[]
+        fromlist, columnValues=[],[self.name]
+        
         if(self.rangeType.upper()=="PREDEFINED"):
 
             if(self.range.upper()=="ALL ALPHABETS"):
@@ -48,36 +53,77 @@ class SimpleColConfig():
             else:
                 print("Range not defined: ",{self.range})        
         
-        columnValues=[self.name]
-        for _ in range(self.noofrows):
-            gen_string=self.rng.random_alphanum(self.minLen, self.maxLen, fromlist)
-            columnValues.append(gen_string)
+            for _ in range(self.noofrows):
+                gen_string=self.rng.random_alphanum(self.minLen, self.maxLen, fromlist)
+                columnValues.append(gen_string)
 
-        # print(columnValues)
-        with open(f"sampleoutput/generated_{int(time.time())}.csv","w+") as fw:
-            writecounter=0
-            for val in columnValues:
-                fw.write(f"{val},\n")
-                print(f"Written {writecounter} of {self.noofrows} values")
-                writecounter+=1
-            fw.close()
+        if self.rangeType.upper()=="FROM-LIST":
+            fromlist=self.range
+            for _ in range(self.noofrows):
+                gen_string=self.rng.element_from_list(fromlist)
+                columnValues.append(gen_string)
 
+        self.savetofile(columnValues,"csv")
+            
+        
     def generatefloat(self):
-        pass
+        fromlist, columnValues=[],[self.name]
+        if self.rangeType.upper()=="START-END":
+           
+            minval,maxval=float(self.range[0]), float(self.range[1])
+            print("start-end", minval, maxval)
+            for _ in range(self.noofrows):
+                columnValues.append(float(self.rng.generate_float(minval,maxval,self.precision)))
+        
+        if self.rangeType.upper()=="FROM-LIST":
+            fromlist=self.range
+            for _ in range(self.noofrows):
+                gen_string=self.rng.element_from_list(fromlist)
+                columnValues.append(float(gen_string))
+
+        self.savetofile(columnValues,"csv")    
+
+    def generateint(self):
+        fromlist, columnValues=[],[self.name]
+        if self.rangeType.upper()=="START-END":
+            minval,maxval=int(self.range[0]), int(self.range[1])
+            for _ in range(self.noofrows):
+                columnValues.append(int(self.rng.generate_number(minval, maxval)))
+        
+        if self.rangeType.upper()=="FROM-LIST":
+            fromlist=self.range
+            for _ in range(self.noofrows):
+                gen_string=self.rng.element_from_list(fromlist)
+                columnValues.append(int(gen_string))
+
+        self.savetofile(columnValues,"csv")
+
 
     def generateinit(self):
         for k in self.jsonconfig:
             print("self."+k+" = "+"jsonconfig[\'"+k+"\']")
 
+    def savetofile(self, columnValuesSet, fileextension="csv"):
+        # print(columnValues)
+        filename = f"sampleoutput/generated_{int(time.time_ns())}.{fileextension}"
+        with open(filename,"w+") as fw:
+            writecounter=0
+            for val in columnValuesSet:
+                fw.write(f"{val},\n")
+                print(f"Written {writecounter} of {self.noofrows} values")
+                writecounter+=1
+            fw.close()
+        print(f"Written to: '{filename}'")
 
-
+# load from sampleconfig file
 sampleconfig={}
 with open("sampleconfigs/simpleconfig1.json","r") as fp:
     sampleconfig=json.load(fp)
     fp.close()
 
-
-config1=SimpleColConfig(sampleconfig,5000)
+# generate config file
+print(sampleconfig)
+config1=SimpleColConfig(sampleconfig,NUM_ROWS)
 config1.construct_generator()
 
 # {"name": "Sample Column 1", "type": "AlphaNumeric", "subtype": "string", "minLen": "5", "maxLen": "20", "rangeType": "predefined", "range": "alphabets", "variance": "ignore", "colPosn": "ignore", "unique": "ignore", "dupFactor": "ignore", "relation": "ignore", "template": "ignore", "custom": "ignore"}
